8e7281b9eb108be049fef1b56c61a82bab9bfa29
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-02 11:19:40 -0300

    Add project files.

---
+++ LanchesMac/Program.cs
@@ -0,0 +1,16 @@
+namespace LanchesMac;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();  
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(WebBuilder =>
+        {
+            WebBuilder.UseStartup<Startup>();
+        });
+}

4110b6a9b826bf3a7034aee6342ac843b1abe26e
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-02 11:31:14 -0300

    feat Delete para a tabela

--- LanchesMac/Migrations/20240618175318_PopularLanches.cs
+++ LanchesMac/Migrations/20240618175318_PopularLanches.cs
@@ -8,14 +8,16 @@ namespace LanchesMac.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemUrl,ImageThumbnailUrl,IsLanchePreferido,Nome,Preco) VALUES(2,'Pão, hambúrguer, ovo, alface','Delicioso pão de hambúrguer com ovo,',1, 'https://initiate.alphacoders.com/images/131/cropped-300-300-1313839.jpg?7153','https://initiate.alphacoders.com/images/131/cropped-300-300-1313839.jpg?7153',1 ,'cheese Salad', 10.50)");
-
+         migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada," +
+                "EmEstoque,ImagemUrl,ImageThumbnailUrl,IsLanchePreferido,Nome,Preco) " +
+                "VALUES(2,'Pão, hambúrguer, ovo, alface','Delicioso pão de hambúrguer com ovo,',1, 'https://initiate.alphacoders.com/images/131/cropped-300-300-1313839.jpg?7153'," +
+                "'https://initiate.alphacoders.com/images/131/cropped-300-300-1313839.jpg?7153',1 ,'cheese Salad', 10.50)");
 
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Lanches");
         }
     }
 }

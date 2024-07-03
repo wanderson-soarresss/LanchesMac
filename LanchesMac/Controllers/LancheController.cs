b21ea98874f2bad6271feda2f0f02524fbbf22ce
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-03 12:05:29 -0300

    Feat : Criando o controller a view para exibir os lanches

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancherepository;
+
+        public LancheController(ILancheRepository lancherepository)
+        {
+            _lancherepository = lancherepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancherepository.Lanches;
+            return View(lanches);
+        }
+    }
+}

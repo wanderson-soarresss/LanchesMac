1b65efe33ee902d1ab68c49043665df73c4ec7ab
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-04 12:13:06 -0300

    Feat : Implementando a View model LancheListViewModel

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Viewmodel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -14,8 +15,13 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancherepository.Lanches;
-            return View(lanches);
+            //var lanches = _lancherepository.Lanches;
+            //return View(lanches);
+            var lancheListViewModel = new LancheListViewModel();
+            lancheListViewModel.Lanches = _lancherepository.Lanches;
+            lancheListViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheListViewModel);
         }
     }
 }
---
+++ LanchesMac/Viewmodel/LancheListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Viewmodel
+{
+    public class LancheListViewModel
+    {
+        public IEnumerable<Lanche> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}

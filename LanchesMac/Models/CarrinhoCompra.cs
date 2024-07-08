ce397e850be3efc9f52eb224a59e053708df29db
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-08 15:02:28 -0300

    update : Limpar carrinho de compras.

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LanchesMac.Models
 {
@@ -71,6 +72,23 @@ namespace LanchesMac.Models
             }
             _context.SaveChanges();
             return quantidadeLocal;
+        }
+        public List<CarrinhoCompraItem> GetCarrinhoCompraItems() 
+        {
+            return CarrinhoCompraItems ??
+                (CarrinhoCompraItems =
+                _context.CarrinhoCompraItens 
+                .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+                .Include(s => s.Lanche)
+                .ToList());   
+        }
+
+        public void LimparCarrinho()
+        {
+            var carrinhoItens = _context.CarrinhoCompraItens
+                                .Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId);
+            _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
+            _context.SaveChanges();
         }
     }
 }

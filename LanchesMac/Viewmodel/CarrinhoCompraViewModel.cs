5add9302e63ef2553d12f345bc0d160a12e6b5c4
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-08 16:06:53 -0300

    feat : Criando o controlador para gerenciar os itens do carrinho de compras.

---
+++ LanchesMac/Viewmodel/CarrinhoCompraViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Viewmodel
+{
+    public class CarrinhoCompraViewModel
+    {
+        public CarrinhoCompra CarrinhoCompra { get; set; }
+        public decimal CarrinhoCompraTotal { get; set; }
+    }
+}

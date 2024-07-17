0bbb8953718d1d18be631083dd30c8e7ff24878a
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-17 15:35:53 -0300

    Feat : Criando a classe Pedido e colocando metodos para vira dados da tabela.

---
+++ LanchesMac/Models/Pedido.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+
+        [Required(ErrorMessage ="Informe o nome")]
+        [StringLength(50)]
+        public string Nome { get; set; }
+
+
+
+        [Required(ErrorMessage = "Informe o sobrenome")]
+        [StringLength(50)]
+        public string Sobrenome { get; set; }
+
+
+
+        [Required(ErrorMessage = "Informe o seu endereço")]
+        [StringLength(100)]
+        [Display(Name = "Endereço")]
+        public string Endereco1 { get; set; }
+
+
+
+        [StringLength(100)]
+        [Display(Name ="Complemento")]
+        public string Endereco2 { get; set; }
+
+
+
+        [Required(ErrorMessage = "Informe o seu cep")]
+        [StringLength(10, MinimumLength = 8)]
+        [Display(Name = "CEP")]
+        public string Cep { get; set; }
+
+
+        [StringLength (10)]
+        public string Estado {  get; set; }
+
+        [StringLength(50)]
+        public string Cidade { get; set; }
+
+
+        [Required(ErrorMessage ="Informe o seu telefone")]
+        [StringLength (25)]
+        [DataType(DataType.PhoneNumber)]
+        public string Telefone { get; set; }
+
+
+    }
+}

0ed7c432faa5d014f11f0487b6068494ccf208b9
Wanderson Gonçalves <141784752+wanderson-soarresss@users.noreply.github.com>
2024-07-17 16:21:07 -0300

    Update.

--- LanchesMac/Models/Pedido.cs
+++ LanchesMac/Models/Pedido.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LanchesMac.Models
 {
@@ -6,7 +7,7 @@ namespace LanchesMac.Models
     {
         public int PedidoId { get; set; }
 
-        [Required(ErrorMessage ="Informe o nome")]
+        [Required(ErrorMessage = "Informe o nome")]
         [StringLength(50)]
         public string Nome { get; set; }
 
@@ -26,7 +27,7 @@ namespace LanchesMac.Models
 
 
         [StringLength(100)]
-        [Display(Name ="Complemento")]
+        [Display(Name = "Complemento")]
         public string Endereco2 { get; set; }
 
 
@@ -37,17 +38,47 @@ namespace LanchesMac.Models
         public string Cep { get; set; }
 
 
-        [StringLength (10)]
-        public string Estado {  get; set; }
+        [StringLength(10)]
+        public string Estado { get; set; }
 
         [StringLength(50)]
         public string Cidade { get; set; }
 
 
-        [Required(ErrorMessage ="Informe o seu telefone")]
-        [StringLength (25)]
+        [Required(ErrorMessage = "Informe o seu telefone")]
+        [StringLength(25)]
         [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Informe o email.")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@" @""^(?!\.)(""""([^""""\r\\]|\\[""""\r\\])*""""|""
+               + @""([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?""
+               + @""[@]((?:[-a-z0-9]+\.)+[a-z]{2,})$""", ErrorMessage = "O email não possui um formato correto")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Column(TypeName ="decimal(18,2")]
+        [Display(Name = "Total do Pedido")]
+        public decimal PedidoTotal { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Name = "itens no Pedido")]
+        public int TotalItensPedido {  get; set; }
+
+
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString ="{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime PedidoEnviado { get; set; }
+
+        [Display(Name = "Data Envio Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime PedidoEntregue { get; set; }
+
+        public List<PedidoDetalhe> PedidoItens { get; set; }
 
 
     }
